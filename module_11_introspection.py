4e341e73833a100e1e6a3a9cf2726022b3a93b99
Danil Terentev <danil.terentyev@gmail.com>
2024-12-04 13:42:58 +0300

    Update module_11_introspection.py

--- module_11_introspection.py
+++ module_11_introspection.py
@@ -1,27 +1,16 @@
 def introspection_info(obj):
-    # тип объекта
-    obj_type = type(obj)
-
-    # атрибуты объекта
-    attributes = dir(obj)
-
-    # методы объекта
-    methods = [attr for attr in attributes if callable(getattr(obj, attr))]
-
-    # модуль, к которому объект принадлежит
-    module = obj_type.__module__
-
-    # другие интересные свойства
-    properties = {}
-    if hasattr(obj, '__dict__'):
-        properties['__dict__'] = obj.__dict__
-    if hasattr(obj, '__name__'):
-        properties['__name__'] = obj.__name__
-    if hasattr(obj, '__doc__'):
-        properties['__doc__'] = obj.__doc__
-
-    info = {'type': obj_type.__name__,'attributes': attributes,'methods': methods,'module': module, 'properties': properties}
-
+    obj_type = type(obj).__name__
+    all_attributes = dir(obj)
+    all_methods = [method for method in all_attributes if
+                   callable(getattr(obj, method)) and not method.startswith('__')]
+    attributes = [attr for attr in all_attributes if not attr.startswith('__') and attr not in all_methods]
+    obj_module = getattr(obj, '__module__', '__main__')
+    info = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': all_methods,
+        'module': obj_module
+    }
     return info
 
 
